d4b4e9d3df02e2ae1862af076c6d0a5f1faeecd3
Marcelo Yates <marceloayates@gmail.com>
2024-07-03 22:28:09 -0500

    reemove assets

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import homepageImage from '../assets/homepage-image.jpg'; // Import your homepage image
+// import homepageImage from '../assets/homepage-image.jpg'; // Import your homepage image
 import styles from '../styles/HomePage.module.css';
+
+const homepageImageURL = "https://res.cloudinary.com/dajuvlxyu/image/upload/v1720063428/homepage-image_rxjzm5.jpg"
 
 const HomePage = () => {
   return (
     <div className={styles.homepageContainer}>
-      <img className={styles.homepageImage} src={homepageImage} alt="Homepage" />
+      <img className={styles.homepageImage} src={homepageImageURL} alt="Homepage" />
     </div>
   );
 };
